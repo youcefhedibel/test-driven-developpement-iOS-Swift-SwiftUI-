98abf943b394c43a1993fef7af3b3430bd929517
youcefhedibel <youyou_dz1@live.fr>
2023-03-09 11:12:31 +0100

    Initial Commit

---
+++ TDD-example/TDD_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD_exampleApp.swift
+//  TDD-example
+//
+//  Created by mac on 9/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct TDD_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
